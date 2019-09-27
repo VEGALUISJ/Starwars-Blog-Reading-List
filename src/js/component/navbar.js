799cc044b0161ae217b61bb2f1fc0fd3a104aff1
Jose Luis Vega <Luisvega.jlvv@gmail.com>
2019-09-27 05:21:15 +0000

    initial commit

---
+++ src/js/component/navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+//include images into your bundle
+
+//create your first component
+export class Navbar extends React.Component {
+	render() {
+		return <div className="prueba">hello world</div>;
+	}
+}

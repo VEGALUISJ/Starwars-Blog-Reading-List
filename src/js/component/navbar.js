8d5db147a9fd55cb51f685ed0754004694631840
Jose Luis Vega <Luisvega.jlvv@gmail.com>
2019-10-02 07:26:56 +0000

    initial commit

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 //include images into your bundle
-import starlogo from "../../img/star-wars-logo2.png";
+import starlogo from "../../img/starwarslogo2.png";
 
 //create your first component
 export class Navbar extends React.Component {
@@ -30,23 +30,23 @@ export class Navbar extends React.Component {
 						<ul className="navbar-nav ml-auto">
 							<li className="nav-item active">
 								<a className="nav-link" href="#">
-									Home |
+									Databank |
 									<span className="sr-only">(current)</span>
 								</a>
 							</li>
 							<li className="nav-item">
 								<a className="nav-link" href="#">
-									About |
+									Films |
 								</a>
 							</li>
 							<li className="nav-item">
 								<a className="nav-link" href="#">
-									Services |
+									Series |
 								</a>
 							</li>
 							<li className="nav-item">
 								<a className="nav-link" href="#">
-									Contact |
+									Videos |
 								</a>
 							</li>
 						</ul>
